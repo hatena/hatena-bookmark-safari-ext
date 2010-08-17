b0a9d33e02ad2ead5159527c9f7b0a049829aa0b
hitode909 <hitode909@gmail.com>
2010-08-17 17:58:00 +0900

    console log

--- hatenabookmark.safariextension/background/request-handler.js
+++ hatenabookmark.safariextension/background/request-handler.js
@@ -50,7 +50,6 @@ Response(function (get, deferred) {
     });
 
     get("UserManager.user", function (ev, matched, dispatch) {
-        console.log(UserManager.user);
         return UserManager.user;
     });
 
