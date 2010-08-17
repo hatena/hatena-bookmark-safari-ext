6f789cae7539e2602238e2755197d664534a2445
mooz <stillpedant@gmail.com>
2010-08-17 18:11:01 +0900

    Supported Cannonical and Images

--- hatenabookmark.safariextension/content/iframe_manager.js
+++ hatenabookmark.safariextension/content/iframe_manager.js
@@ -7,6 +7,8 @@
 
     var iframe;
 
+
+
     window.addEventListener("message", function (ev) {
         var res;
 
@@ -22,11 +24,54 @@
     }, false);
 
     function getInfo(ev) {
-        var res   = {};
+        var res = {};
+
         res.url   = location.href;
         res.title = document.title;
 
+        var cannonical = getCannonical();
+        if (cannonical)
+            res.cannonical = cannonical;
+
+        var images = getImages();
+        if (images && images.length)
+            res.images = images;
+
         return res;
+    }
+
+    function getCannonical() {
+            var link = document.evaluate(
+                '/h:html/h:head/h:link[translate(@rel, "CANONICAL", "canonical") = "canonical"]',
+                document,
+                function () { return document.documentElement.namespaceURI || ""; },
+                XPathResult.FIRST_ORDERED_NODE_TYPE,
+                null
+            ).singleNodeValue;
+        if (!link || !link.href) return null;
+        var url = link.href;
+        if (location.href == url) return null;
+        return url;
+    }
+
+    function getImages() {
+        var images = Array.prototype.filter.call(
+            document.getElementsByTagName("img"),
+            function (img) { return (img instanceof HTMLImageElement); }
+        );
+
+        var maxCount = 20;
+        if (images.length > maxCount) {
+            images = images.map(function (image, index) {
+                var size = Math.min(image.naturalWidth,
+                                    image.naturalHeight);
+                return { image: image, size: size, index: index };
+            }).sort(function (a, b) { return b.size - a.size; })
+                .slice(0, maxCount)
+                .sort(function(a, b) { return a.index - b.index; })
+                .map(function (item) { return item.image; });
+        }
+        return images.filter(function(image) { return image && image.src; }).map(function(image) { return image.src; });
     }
 
     function closeIframe(ev) {
