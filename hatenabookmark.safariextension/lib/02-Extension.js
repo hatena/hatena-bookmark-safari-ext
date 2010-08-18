f1dc4deabb374c993bfd28aa386912116e239058
mooz <stillpedant@gmail.com>
2010-08-18 11:34:04 +0900

    fetch identifier from baseURI

--- hatenabookmark.safariextension/lib/02-Extension.js
+++ hatenabookmark.safariextension/lib/02-Extension.js
@@ -1,5 +1,13 @@
 var Extension = {
-    get id () { return 'jp.ne.hatena.b-YCM7KLTD3K'; },
+    get id () {
+        if (!this._id) {
+            var uri = safari.extension.baseURI;
+            var id  = uri.match("safari-extension://\(jp.ne.hatena.b-[a-zA-Z0-9]+\)/")[1];
+            this._id = id;
+        }
+
+        return this._id;
+    },
 
     getIdentifier: function (name) {
         return this.id + " " + name;
