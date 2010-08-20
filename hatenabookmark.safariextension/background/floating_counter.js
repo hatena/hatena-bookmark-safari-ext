b7dc2ab52396615a5f8ab0135e426a42a33f5ee3
hitode909 <hitode909@gmail.com>
2010-08-20 16:41:57 +0900

    無効のときフローティング出さないとか

--- hatenabookmark.safariextension/background/floating_counter.js
+++ hatenabookmark.safariextension/background/floating_counter.js
@@ -13,39 +13,49 @@
 
     function show(entry) {
         $(function() {
-            if (Config.get("content.bookmarkcounter.enabled")) $("#count a")
-                .text(entry.count)
-                .attr('href', 'http://b.hatena.ne.jp/entry/' + entryURL)
-                .click(function() {
-                    Connect().send("PopupManager.show", { url : entryURL, view : "comment" }).recv(function (ev) {}).close();
-                    return false;
+            if (!(Config.get("content.bookmarkcounter.enabled") || Config.get("content.fans.enabled"))) return;
+
+            if (Config.get("content.bookmarkcounter.enabled")) {
+                $("#count a")
+                    .text(entry.count)
+                    .attr('href', 'http://b.hatena.ne.jp/entry/' + entryURL)
+                    .click(function() {
+                        Connect().send("PopupManager.show", { url : entryURL, view : "comment" }).recv(function (ev) {}).close();
+                        return false;
+                    });
+            } else {
+                $("#count a").remove();
+            }
+
+            if (Config.get("content.fans.enabled")) {
+                entry.favorites.reverse().forEach(function (fav) {
+                    var link  = "http://b.hatena.ne.jp/" + fav.name + "/" + fav.timestamp.replace(/\//g, '') + "#bookmark-" + entry.eid;
+                    var lines = [fav.name];
+
+                    if (fav.body) lines.push(fav.body);
+                    if (fav.tags.length) lines.push(fav.tags.join(', '));
+                    lines.push(fav.timestamp);
+
+                    var iconContainer = $("<a>");
+                    iconContainer.attr({
+                        href: link,
+                        title: lines.join("\n"),
+                        target: '_blank'
+                    });
+
+                    var icon = $("<img>");
+                    icon.attr({
+                        width: 16,
+                        height: 16,
+                        src: "http://www.st-hatena.com/users/" + fav.name.substring(0, 2) + "/" + fav.name + "/profile_s.gif"
+                    });
+
+                    iconContainer.append(icon);
+                    $("#favs").append(iconContainer);
                 });
-
-            if (Config.get("content.fans.enabled")) entry.favorites.reverse().forEach(function (fav) {
-                var link  = "http://b.hatena.ne.jp/" + fav.name + "/" + fav.timestamp.replace(/\//g, '') + "#bookmark-" + entry.eid;
-                var lines = [fav.name];
-
-                if (fav.body) lines.push(fav.body);
-                if (fav.tags.length) lines.push(fav.tags.join(', '));
-                lines.push(fav.timestamp);
-
-                var iconContainer = $("<a>");
-                iconContainer.attr({
-                    href: link,
-                    title: lines.join("\n"),
-                    target: '_blank'
-                });
-
-                var icon = $("<img>");
-                icon.attr({
-                    width: 16,
-                    height: 16,
-                    src: "http://www.st-hatena.com/users/" + fav.name.substring(0, 2) + "/" + fav.name + "/profile_s.gif"
-                });
-
-                iconContainer.append(icon);
-                $("#favs").append(iconContainer);
-            });
+            } else {
+                $("#favs").remove();
+            }
 
             var padding = 8;
             window.parent.postMessage('resizeCounter?width=' +
