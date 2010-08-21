06ba3212c1c2fa249bb811f2b9388bafc43e8e5d
mooz <stillpedant@gmail.com>
2010-08-21 22:07:43 +0900

    Sync extension's settings value with localStorage

--- hatenabookmark.safariextension/background/config-setting.js
+++ hatenabookmark.safariextension/background/config-setting.js
@@ -1,10 +1,9 @@
-
 (function() {
-    var defaults = {
-        // ================================================== //
-        // safari.extension.settings
-        // ================================================== //
 
+    // ================================================== //
+    // safari.extension.settings
+    // ================================================== //
+    var extensionSettings = {
         'popup.window.width': {
             'default': 550,
             type: 'unsignedInt'
@@ -26,12 +25,13 @@
         'popup.commentviewer.autodetect.threshold': 15,
         'contextmenu.enabled': true,
         'content.fans.enabled': true,
-        'content.bookmarkcounter.enabled': true,
+        'content.bookmarkcounter.enabled': true
+    };
 
-        // ================================================== //
-        // localStorage
-        // ================================================== //
-
+    // ================================================== //
+    // localStorage
+    // ================================================== //
+    var tmpSettings = {
         'popup.search.lastWord': '',
         'popup.commentviewer.togglehide': false,
         'popup.tags.showAllTags': false,
@@ -41,8 +41,20 @@
         'popup.bookmark.lastCommentValue': {},
         'popup.lastView': 'comment'
     };
-    Object.keys(defaults).forEach(function(key) {
-        Config.append(key, defaults[key]);
+
+    function inform(settings) {
+        Object.keys(settings).forEach(function (k) { Config.append(k, settings[k]); });
+    }
+
+    // 管理対象となる項目とその他諸々の情報をConfig クラスへ知らせる
+    inform(extensionSettings);
+    inform(tmpSettings);
+
+    // safari.extension.settings で管理される項目については, localStorage へ書き込む (同期をとる)
+    Object.keys(extensionSettings).forEach(function (key) {
+        var value = safari.extension.settings.getItem(key);
+        console.log("Sync " + key);
+        console.log(Config.get(key) + " => " + value);
+        Config.set(key, value);
     });
 })();
-
