63e610b50457459e979b4a201bf299113bda62c4
hitode909 <hitode909@gmail.com>
2010-08-24 20:45:23 +0900

    log

--- hatenabookmark.safariextension/background/extension-settings-listener.js
+++ hatenabookmark.safariextension/background/extension-settings-listener.js
@@ -1,4 +1,3 @@
 safari.extension.settings.addEventListener("change", function (ev) {
-    console.log(sprintf("Settings [%s] changed (%s => %s)", ev.key, ev.oldValue, ev.newValue));
     Config.set(ev.key, ev.newValue);
 }, false);
