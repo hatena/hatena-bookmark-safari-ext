e7b1ec2ce53a964abfde10dc6fa36868fe1326ce
nanto_vi <nanto@moon.email.ne.jp>
2011-05-20 17:33:11 +0900

    canonicalに移動した後にenterキーでsubmitできなくなってたのを修正 (Thanks to hitode909)

--- hatenabookmark.safariextension/background/popup.js
+++ hatenabookmark.safariextension/background/popup.js
@@ -527,17 +527,6 @@ var View = {
             getInformation().next(function(info) {
                 self.loadByInformation(info);
             });
-
-            var form = this.form;
-            if (!form.data('keypressBound')) {
-                form.data('keypressBound', true);
-                form.keypress(function(e) {
-                    if (e.keyCode !== 13 || e.target !== self.commentEL.get(0))
-                        return;
-                    $('#edit-submit').click();
-                    return false;
-                });
-            }
         },
         clearView: function() {
             this.container.empty();
@@ -714,6 +703,17 @@ var View = {
                 input.setSelectionRange(index, index);
                 return false;
             });
+
+            var form = this.form;
+            if (!form.data('keypressBound')) {
+                form.data('keypressBound', true);
+                form.keypress(function(e) {
+                    if (e.keyCode !== 13 || e.target !== self.commentEL.get(0))
+                        return;
+                    $('#edit-submit').click();
+                    return false;
+                });
+            }
 
             this.form.show();
             this.commentEL.focus();
